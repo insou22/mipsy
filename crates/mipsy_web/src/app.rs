1a376e177b777af70650fcfec42485fae3976c3a
SpanishPear <shrey.somaiya@gmail.com>
2021-09-06 01:04:12 +1000

    restructure + initial splitjs attempts

--- crates/mipsy_web/src/app.rs
+++ crates/mipsy_web/src/app.rs
@@ -1,7 +1,14 @@
-use mipsy_lib::RuntimeHandler;
+use crate::components::{navbar::NavBar, pagebackground::PageBackground};
+use mipsy_lib::{Binary, InstSet, RuntimeHandler};
 use mipsy_parser::TaggedFile;
-use yew::{prelude::*, services::{ConsoleService, ReaderService, reader::{FileData, ReaderTask}}, web_sys::File};
-use crate::components::{pagebackground::PageBackground, navbar::NavBar};
+use yew::{
+    prelude::*,
+    services::{
+        reader::{FileData, ReaderTask},
+        ConsoleService, ReaderService,
+    },
+    web_sys::File,
+};
 
 fn crimes<T>() -> T {
     panic!()
@@ -12,11 +19,11 @@ struct Handler {
 }
 
 impl RuntimeHandler for Handler {
-    fn sys1_print_int   (&mut self, val: i32) {
-        ConsoleService::info(val.to_string().as_str()); 
+    fn sys1_print_int(&mut self, val: i32) {
+        ConsoleService::info(val.to_string().as_str());
     }
 
-    fn sys2_print_float (&mut self, val: f32) {
+    fn sys2_print_float(&mut self, val: f32) {
         todo!()
     }
 
@@ -28,51 +35,66 @@ impl RuntimeHandler for Handler {
         todo!()
     }
 
-    fn sys5_read_int    (&mut self) -> i32 {
+    fn sys5_read_int(&mut self) -> i32 {
         42
     }
 
-    fn sys6_read_float  (&mut self) -> f32 {
+    fn sys6_read_float(&mut self) -> f32 {
         todo!()
     }
 
-    fn sys7_read_double (&mut self) -> f64 {
+    fn sys7_read_double(&mut self) -> f64 {
         todo!()
     }
 
-    fn sys8_read_string (&mut self, max_len: u32) -> String {
+    fn sys8_read_string(&mut self, max_len: u32) -> String {
         todo!()
     }
 
-    fn sys9_sbrk        (&mut self, val: i32) {
+    fn sys9_sbrk(&mut self, val: i32) {
         todo!()
     }
 
-    fn sys10_exit       (&mut self) {
+    fn sys10_exit(&mut self) {
         self.exited = true;
     }
 
-    fn sys11_print_char (&mut self, val: char) {
+    fn sys11_print_char(&mut self, val: char) {
         ConsoleService::info(val.to_string().as_str());
     }
 
-    fn sys12_read_char  (&mut self) -> char {
+    fn sys12_read_char(&mut self) -> char {
         todo!()
     }
 
-    fn sys13_open       (&mut self, path: String, flags: mipsy_lib::flags, mode: mipsy_lib::mode) -> mipsy_lib::fd {
+    fn sys13_open(
+        &mut self,
+        path: String,
+        flags: mipsy_lib::flags,
+        mode: mipsy_lib::mode,
+    ) -> mipsy_lib::fd {
         todo!()
     }
 
-    fn sys14_read       (&mut self, fd: mipsy_lib::fd, buffer: mipsy_lib::void_ptr, len: mipsy_lib::len) -> mipsy_lib::n_bytes {
+    fn sys14_read(
+        &mut self,
+        fd: mipsy_lib::fd,
+        buffer: mipsy_lib::void_ptr,
+        len: mipsy_lib::len,
+    ) -> mipsy_lib::n_bytes {
         todo!()
     }
 
-    fn sys15_write      (&mut self, fd: mipsy_lib::fd, buffer: mipsy_lib::void_ptr, len: mipsy_lib::len) -> mipsy_lib::n_bytes {
+    fn sys15_write(
+        &mut self,
+        fd: mipsy_lib::fd,
+        buffer: mipsy_lib::void_ptr,
+        len: mipsy_lib::len,
+    ) -> mipsy_lib::n_bytes {
         todo!()
     }
 
-    fn sys16_close      (&mut self, fd: mipsy_lib::fd) {
+    fn sys16_close(&mut self, fd: mipsy_lib::fd) {
         todo!()
     }
 
@@ -80,7 +102,7 @@ impl RuntimeHandler for Handler {
         self.exited = true;
     }
 
-    fn breakpoint       (&mut self) {
+    fn breakpoint(&mut self) {
         todo!()
     }
 }
@@ -91,12 +113,27 @@ pub enum Msg {
     Run,
 }
 
+pub struct RunningState {
+    file: String,
+    binary: Binary,
+    decompiled: String,
+}
+
+pub enum State {
+    NoFile,
+    // File is loaded, and compiled?
+    Running(RunningState),
+}
+
 pub struct App {
     // `ComponentLink` is like a reference to a component.
     // It can be used to send messages to the component
     link: ComponentLink<Self>,
-    file: Option<String>,
-    jeff: Vec<ReaderTask>
+
+    // a tasks vec to keep track of any file uploads
+    tasks: Vec<ReaderTask>,
+    state: State,
+    inst_set: InstSet,
 }
 
 impl Component for App {
@@ -104,24 +141,29 @@ impl Component for App {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, file: None, jeff: vec![] }
+        Self {
+            link,
+            state: State::NoFile,
+            tasks: vec![],
+            inst_set: mipsy_codegen::instruction_set!("../../mips.yaml"),
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-
         match msg {
             Msg::FileChanged(file) => {
                 ConsoleService::info("file changed msg");
                 // FIXME -- check result
-                let result = ReaderService::read_file(file, self.link.callback(|file_data| Msg::FileRead(file_data))); 
-                
+                let result = ReaderService::read_file(
+                    file,
+                    self.link.callback(|file_data| Msg::FileRead(file_data)),
+                );
+
                 match result {
-                    Ok(service) => {
-                        self.jeff.push(service);
-                    } 
-                
+                    Ok(service) => self.tasks.push(service),
+
                     Err(err) => {
-                        ConsoleService::error(&format!("{:?}",err));
+                        ConsoleService::error(&format!("{:?}", err));
                     }
                 }
                 false
@@ -129,43 +171,64 @@ impl Component for App {
             Msg::FileRead(file_data) => {
                 ConsoleService::info(&format!("{:?}", file_data));
                 // TODO -- this should not be lossy
-                self.file = Some(String::from_utf8_lossy(&file_data.content).to_string()); 
+
+                let file = String::from_utf8_lossy(&file_data.content).to_string();
+
+                let compiled = mipsy_lib::compile(
+                    &self.inst_set,
+                    vec![TaggedFile::new(None, file.as_str())],
+                    8,
+                );
+
+                match compiled {
+                    Ok(binary) => {
+                        let decompiled = mipsy_lib::decompile(&self.inst_set, &binary);
+                        match self.state {
+                            // Overwrite existing state,
+                            State::NoFile | State::Running(_) => {
+                                self.state = State::Running(RunningState {
+                                    file,
+                                    binary,
+                                    decompiled,
+                                })
+                            }
+                        }
+                    }
+
+                    Err(err) => {
+                        // this should eventually go to a new state
+                        // errorstate
+                        // and show on page
+                        ConsoleService::error(&format!("{:?}", err));
+                    }
+                }
+
                 true
             }
 
             Msg::Run => {
-               
-                ConsoleService::time_named("compile");
-                let inst_set = mipsy_codegen::instruction_set!("../../mips.yaml");
-                // hardcoded tabsize 8
-                let compiled = mipsy_lib::compile(&inst_set, vec![TaggedFile::new(None, self.file.as_deref().unwrap())], 8);
-                
-                match compiled {
-                    Ok(binary) => {
-                        let text = mipsy_lib::decompile(&inst_set, &binary);
-                        self.file = Some(text);
-                        let mut runtime = mipsy_lib::runtime(&binary, &[]);
-                        let mut rh = Handler {
-                            exited: false,
-                        };
-                        loop {
-                            runtime.step(&mut rh);
-                            if rh.exited {
-                                break;
-                            }
+                if let State::Running(RunningState {
+                    file,
+                    binary,
+                    decompiled,
+                }) = &self.state
+                {
+                    let mut rh = Handler { exited: false };
+                    let mut runtime = mipsy_lib::runtime(&binary, &[]);
+                    loop {
+                        runtime.step(&mut rh);
+                        if rh.exited {
+                            break;
                         }
-
                     }
-
-                    Err(err) => {
-                        ConsoleService::error(&format!("{:?}",err));
-                    }
+                } else {
+                    ConsoleService::error("No File, cannot run");
+                    return false;
                 }
-                ConsoleService::time_named_end("compile");
-                true 
+
+                true
             }
         }
-        
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -174,52 +237,61 @@ impl Component for App {
         // This component has no properties so we will always return "false".
         false
     }
-    
+
     fn view(&self) -> Html {
         let onchange = self.link.batch_callback(|event| {
             ConsoleService::info("onchange fired");
             match event {
                 ChangeData::Files(file_list) => {
                     if let Some(file) = file_list.item(0) {
-                        ConsoleService::info(&format!("{:?}", file.name() ));
+                        ConsoleService::info(&format!("{:?}", file.name()));
                         Some(Msg::FileChanged(file))
                     } else {
                         None
                     }
-                },
-                _ => None, 
-
-            } 
-        } );
+                }
+                _ => None,
+            }
+        });
 
         let run_onclick = self.link.callback(|_| {
             ConsoleService::info("Run fired");
             Msg::Run
         });
 
-        let text = {
-            if let Some(data) = self.file.as_deref() {
-                data
-            } else {
-                "No File Loaded :("
-            }
+        let inner = match &self.state {
+            &State::NoFile => "no file loaded".into(),
+            &State::Running(ref state) => self.render_running(state),
         };
-
-        ConsoleService::info(&format!("render {:?}", text));
         
         html! {
-            <PageBackground>
-                <NavBar load_onchange=onchange run_onclick=run_onclick />
-                <p>
-                {text.split("\n").map(|line| {
-                    html! {
-                        <p>
-                            {line}
-                        </p>
-                    }
-                }).collect::<Vec<_>>()}
-                </p>
-            </PageBackground>
+            <>
+                <PageBackground>
+                    <NavBar load_onchange=onchange run_onclick=run_onclick />
+                    <div id="text" class="px-2 resize-y border-2 border-black">
+                        { inner }
+                    </div>
+                    <div id="output" class="border-2 border-green">
+                    </div>
+                </PageBackground>
+
+            </>
         }
+    }
+}
+
+impl App {
+    fn render_running(&self, state: &RunningState) -> Html {
+        state
+            .file
+            .split("\n")
+            .map(|line| {
+                html! {
+                    <p>
+                        {line}
+                    </p>
+                }
+            })
+            .collect::<Html>()
     }
 }
--- crates/mipsy_web/src/main.rs
+++ crates/mipsy_web/src/main.rs
@@ -1,12 +1,23 @@
 #![recursion_limit = "1024"]
-#![allow(clippy::clippy::large_enum_variant)]
+#![allow(clippy::large_enum_variant)]
 
+use yew::{ App };
 pub mod app;
 pub mod components;
 pub mod pages;
 
-use app::App;
+use app::App as Application;
+
 
 pub fn main() {
-    yew::start_app::<App>();
+
+    yew::initialize();
+    
+    let document = yew::utils::document();
+    let element = document.query_selector("#mount_application").unwrap().unwrap();
+    let app: yew::App<Application> = App::new();
+    app.mount(element);
+
+
+    yew::run_loop();
 }
