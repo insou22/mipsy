ccf60fda6f356b7ab247e1f7d675de2a6f0feec2
insou22 <zac.kologlu@gmail.com>
2021-10-05 16:07:24 +1100

    make dev instead build with profiling, as wasm fails to build (50,000 fn-local limit) without optimisations

--- crates/mipsy_web/scripts/build_dev.sh
+++ crates/mipsy_web/scripts/build_dev.sh
@@ -1,2 +1,2 @@
 tailwindcss -c ./tailwind.config.js -o ./static/tailwind.css
-wasm-pack build --target no-modules --out-name wasm --out-dir ./static --no-typescript --dev
+wasm-pack build --target no-modules --out-name wasm --out-dir ./static --no-typescript --profiling
