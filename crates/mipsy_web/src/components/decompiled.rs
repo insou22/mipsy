921510cd9b05a08afde36a30f9faf82fc03ec882
SpanishPear <shrey.somaiya@gmail.com>
2022-02-27 11:50:18 +1100

    fix #149 - does not yet step through kernel on load

--- crates/mipsy_web/src/components/decompiled.rs
+++ crates/mipsy_web/src/components/decompiled.rs
@@ -1,5 +1,6 @@
 use crate::pages::main::state::RunningState;
 use yew::{function_component, html, Properties};
+use log::info;
 
 #[derive(Properties, PartialEq)]
 pub struct DecompiledProps {
@@ -22,7 +23,8 @@ pub fn render_decompiled(props: &DecompiledProps) -> Html {
                 let should_highlight = if item.starts_with("0x") {
                     // the actual hex address lives from 2-10, 01 are 0x
                     let source_instr = u32::from_str_radix(&item[2..10], 16).unwrap_or(0);
-                    source_instr == runtime_instr
+                    // instr we want to highlight is the one before the current one
+                    source_instr == runtime_instr.saturating_sub(4)
                 } else {
                     false
                 };
