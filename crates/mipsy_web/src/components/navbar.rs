c4cecb7e57cc52f5d66f8740f16e2375cf291e05
Dylan Brotherston <dylanbbbrother@gmail.com>
2022-03-01 17:37:07 +1100

    Add save button to mipsy web interface (#165)
    * allow cmd (meta) key for mac
    
    * maybe a save button
    
    * pass is_saved to navbar
    
    * saving doesn't need the code to be compiled
    
    * use a more concise way of formatting
    
    * make the save button icon behave the same as other icons
    
    * make buttons smaller + add save button title
    
    * fix #166 
    
    Co-authored-by: SpanishPear <shrey.somaiya@gmail.com>

--- crates/mipsy_web/src/components/navbar.rs
+++ crates/mipsy_web/src/components/navbar.rs
@@ -1,7 +1,7 @@
 use crate::pages::main::state::{MipsState, RunningState};
 use crate::{
     pages::main::{app::NUM_INSTR_BEFORE_RESPONSE, state::State},
-    worker::Worker,
+    worker::{Worker, WorkerRequest},
 };
 use derivative::Derivative;
 use log::{info, trace};
@@ -18,6 +18,7 @@ pub struct NavBarProps {
     pub file_loaded: bool,
     pub waiting_syscall: bool,
     pub state: UseStateHandle<State>,
+    pub is_saved: UseStateHandle<bool>,
     #[derivative(PartialEq = "ignore")]
     pub worker: UseBridgeHandle<Worker>,
     pub filename: UseStateHandle<Option<String>>,
@@ -35,9 +36,35 @@ struct Icon {
 fn icons(props: &NavBarProps) -> Vec<Icon> {
     let icons = vec![
         Icon {
+            label: String::from("Save"),
+            html: html! {
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M17.85 3.15l-2.99-3A.508.508 0 0 0 14.5 0H1.4A1.417 1.417 0 0 0 0 1.43v15.14A1.417 1.417 0 0 0 1.4 18h15.2a1.417 1.417 0 0 0 1.4-1.43V3.5a.47.47 0 0 0-.15-.35zM2 5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1zm7 11a4 4 0 1 1 4-4 4 4 0 0 1-4 4z"/>
+                </svg>
+            },
+            title: String::from("Save and compile the current file"),
+            callback: Some({
+                let file = props.file.clone();
+                let worker = props.worker.clone();
+                let filename = props.filename.clone();
+                let is_saved = props.is_saved.clone();
+                Callback::from(move |_| {
+                    trace!("Save button clicked");
+                    is_saved.set(true);
+                    let updated_content = crate::get_editor_value();
+                    let clone = updated_content.clone();
+                    crate::set_localstorage_file_contents(&updated_content);
+                    crate::set_localstorage_filename(&filename.as_deref().unwrap_or("Untitled"));
+                    file.set(Some(updated_content));
+                    worker.send(WorkerRequest::CompileCode(clone));
+                })
+            }),
+            disable_override: true,
+        },
+        Icon {
             label: String::from("Run"),
             html: html! {
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
                 </svg>
             },
@@ -75,7 +102,7 @@ fn icons(props: &NavBarProps) -> Vec<Icon> {
         Icon {
             label: String::from("Reset"),
             html: html! {
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                      <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
                 </svg>
             },
@@ -98,7 +125,7 @@ fn icons(props: &NavBarProps) -> Vec<Icon> {
         Icon {
             label: String::from("Kill"),
             html: html! {
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
                 </svg>
             },
@@ -120,7 +147,7 @@ fn icons(props: &NavBarProps) -> Vec<Icon> {
         Icon {
             label: String::from("Step Back"),
             html: html! {
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                 </svg>
             },
@@ -153,7 +180,7 @@ fn icons(props: &NavBarProps) -> Vec<Icon> {
         Icon {
             label: String::from("Step Next"),
             html: html! {
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                 </svg>
             },
@@ -186,7 +213,7 @@ fn icons(props: &NavBarProps) -> Vec<Icon> {
         Icon {
             label: String::from("Download"),
             html: html! {
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 20 20" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" fill="none" viewBox="0 0 20 20" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                 </svg>
             },
@@ -225,7 +252,7 @@ pub fn render_navbar(props: &NavBarProps) -> Html {
           <div class="w-full block flex-grow flex items-center w-auto">
             <div class="flex-grow flex flex-row">
               <label tabindex=0 for="load_file" class="mr-2 text-sm flex place-items-center flex-row inline-block cursor-pointer px-3 py-3 leading-none border rounded text-black border-black hover:border-transparent hover:text-teal-500 hover:bg-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
                   <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
                 </svg>
