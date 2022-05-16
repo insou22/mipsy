3223b25641fd92bb1bce70eec43a44b15269c50d
SpanishPear <shrey.somaiya@gmail.com>
2022-05-16 10:49:41 +1000

    upadted deploy script

--- crates/mipsy_web/scripts/deploy.sh
+++ crates/mipsy_web/scripts/deploy.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-# Note, this presumes you have an ssh alias `cse` 
+#!/usr/bin/env bash 
+# This script will build and deploy to UNSW CSE servers
+# presumes you have an ssh alias 'cse'
 
 
 if ! command -v trunk 2>&1 >/dev/null;
@@ -14,21 +15,27 @@ then
 	exit
 fi
 
-trunk build --release
+CSE_ACCOUNT=
+PUBLIC_URL="/"
+while true; do
+    case "$1" in 
+        --cse_account=*)
+            val=${1#*=}
+            CSE_ACCOUNT="$val"; shift; 
+            ;;
+        --public_url=*)             
+            val=${1#*=}
+            PUBLIC_URL="$val"; shift; 
+            ;;
+        * ) break ;;
+    esac
+done
 
-if [ "$1" = "--push=shreys" ]; then
+trunk build --release --public-url=$PUBLIC_URL
 
-    scp -r dist/* cse:~/web/mipsy/
-
-fi
-
-if [ "$1" = "--push=cs1521" ]; then
-
-    scp -r dist/* cse:~cs1521/web/mipsy/
-
-fi
-
-if [ "$1" = "--push=both" ]; then
+if [ "$CSE_ACCOUNT" = "both" ]; then
     scp -r dist/* cse:~cs1521/web/mipsy
     scp -r dist/* cse:~/web/mipsy
+else
+    scp -r dist/* cse:~$CSE_ACCOUNT/web/mipsy/
 fi
