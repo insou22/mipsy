56d58ec0cc27a1fbf3994f682221e17793123b44
Josh Harcombe <joshharc@gmail.com>
2022-09-21 09:44:15 +1000

    fix help for watchpoints

--- crates/mipsy_interactive/src/interactive/commands/watchpoint.rs
+++ crates/mipsy_interactive/src/interactive/commands/watchpoint.rs
@@ -64,11 +64,10 @@ fn get_long_help() -> String {
         "A collection of commands for managing watchpoints. Available {10}s are:\n\n\
          {0} {2}    : insert/delete a watchpoint\n\
          {1} {3}\n\
-         {0} {11} : insert a temporary watchpoint that deletes itself after being hit\n\
          {0} {5}    : enable/disable an existing watchpoint\n\
          {1} {6}\n\
          {1} {7}\n\
-         {0} {12}    : ignore a watchpoint for a specified number of hits\n\
+         {0} {11}    : ignore a watchpoint for a specified number of hits\n\
          {0} {4}      : list currently set watchpoints\n\n\
          {8} {9} will provide more information about the specified subcommand.
         ",
@@ -83,42 +82,33 @@ fn get_long_help() -> String {
         "help watchpoint".bold(),
         "<subcommand>".purple().bold(),
         "<subcommand>".purple(),
-        "temporary".purple(),
         "ignore".purple(),
     )
 }
 
 fn watchpoint_insert(state: &mut State, label: &str, args: &[String], remove: bool) -> Result<String, CommandError> {
     if label == "__help__" {
-        // TODO(joshh): fix help for watchpoints, remove temporary etc
         return Ok(
             format!(
-                "Usage: {10} {11} {2}\n\
+                "Usage: {5} {6} {2} {7}\n\
                  {0}s or {1}s a watchpoint at the specified {2}.\n\
-                 {2} may be: a decimal address (`4194304`), a hex address (`{3}400000`), or a label (`{4}`).\n\
-                 If you are removing a watchpoint, you can also use its id (`{5}`).\n\
-                 {6} must be `i`, `in`, `ins`, `insert`, or `add` to insert the watchpoint, or\n\
+                 {2} may be: a register name (`$t0`, `t0`), or a register number (`$14`, `14`).\n\
+                 If you are removing a watchpoint, you can also use its id (`{3}`).\n\
+                 {4} must be `i`, `in`, `ins`, `insert`, or `add` to insert the watchpoint, or\n\
             \x20             `del`, `delete`, `rm` or `remove` to remove the watchpoint.\n\
-                 If {12}, `tmp`, or `temp` is provided as the {6}, the watchpoint will\n\
-                 be created as a temporary watchpoint, which automatically deletes itself after being hit.\n\
-                 If {6} is none of these option, it defaults to inserting a watchpoint at {6}.\n\
+                 If {4} is none of these option, it defaults to inserting a watchpoint at {4}.\n\
                  When running or stepping through your program, a watchpoint will cause execution to\n\
-                 pause temporarily, allowing you to debug the current state.\n\
-                 May error if provided a label that doesn't exist.\n\
-              \n{7}{8} you can also use the `{9}` MIPS instruction in your program's code!",
+                 pause temporarily when the specified register is read from or written to,\n\
+                 allowing you to debug the current state.\n\
+                 May error if provided a register that doesn't exist.",
                 "<insert>".magenta(),
                 "<delete>".magenta(),
-                "<address>".magenta(),
-                "0x".yellow(),
-                "main".yellow().bold(),
+                "<register>".magenta(),
                 "!3".blue(),
                 "<subcommand>".magenta(),
-                "tip".yellow().bold(),
-                ":".bold(),
-                "break".bold(),
                 "watchpoint".yellow().bold(),
-                "{insert, delete, temporary}".purple(),
-                "<temporary>".purple(),
+                "{insert, delete}".purple(),
+                "{read, write, read/write}".purple(),
             )
         )
     }
@@ -213,11 +203,8 @@ fn watchpoint_list(state: &State, label: &str, _args: &[String]) -> Result<Strin
     if label == "__help__" {
         return Ok(
             format!(
-                "Lists currently set watchpoints.\n\
-                 When running or stepping through your program, a watchpoint will cause execution to\n\
-                 pause temporarily, allowing you to debug the current state.\n\
-                 May error if provided a label that doesn't exist.",
-            ),
+                "Lists currently set watchpoints."
+            )
         )
     }
 
@@ -279,21 +266,15 @@ fn watchpoint_toggle(state: &mut State, label: &str, mut args: &[String], enable
     if label == "__help__" {
         return Ok(
             format!(
-                "Usage: {8} {9} {3}\n\
+                "Usage: {5} {6} {3}\n\
                  {0}s, {1}s, or {2}s a watchpoint at the specified {3}.\n\
-                 {3} may be: a decimal address (`4194304`), a hex address (`{4}400000`),\n\
-        \x20                 a label (`{5}`), or an id (`{6}`).\n\
-                 watchpoints that are disabled do not trigger when they are hit.\n\
-                 watchpoints caused by the `{7}` instruction in code cannot be disabled.
-                ",
+                 {3} may be: a register name (`$t0`, `t0`), a register number (`$14`, 14), or an id (`{4}`).\n\
+                 watchpoints that are disabled do not trigger when they are hit.",
                 "<enable>".purple(),
                 "<disable>".purple(),
                 "<toggle>".purple(),
-                "<address>".purple(),
-                "0x".yellow(),
-                "main".yellow().bold(),
+                "<register>".purple(),
                 "!3".blue(),
-                "break".bold(),
                 "watchpoint".yellow().bold(),
                 "{enable, disable, toggle}".purple(),
             )
@@ -354,19 +335,14 @@ fn watchpoint_ignore(state: &mut State, label: &str, mut args: &[String]) -> Res
     if label == "__help__" {
         return Ok(
             format!(
-                "Usage: {6} {7} {1} {0}\n\
-                 {7}s a watchpoint at the specified {1} for the next {0} hits.\n\
-                 {1} may be: a decimal address (`4194304`), a hex address (`{2}400000`),\n\
-        \x20                 a label (`{3}`), or an id (`{4}`).\n\
+                "Usage: {3} {4} {1} {0}\n\
+                 {4}s a watchpoint at the specified {1} for the next {0} hits.\n\
+                 {1} may be: a register name (`$t0`, `t0`), a register number (`$14`, 14), or an id (`{2}`).\n\
                  watchpoints that are ignored do not trigger when they are hit.\n\
-                 watchpoints caused by the `{5}` instruction in code cannot ignored.
                 ",
                 "<ignore count>".purple(),
-                "<address>".purple(),
-                "0x".yellow(),
-                "main".yellow().bold(),
+                "<register>".purple(),
                 "!3".blue(),
-                "break".bold(),
                 "watchpoint".yellow().bold(),
                 "ignore".purple(),
             )
