7870c205556caba41b5d984814a64199f5e8abaf
SpanishPear <shrey.somaiya@gmail.com>
2023-09-10 21:39:19 +1000

    remove accidental duplication of file

--- src/bin/worker.rs
+++
@@ -1,7 +0,0 @@
-use mipsy_web::worker::Worker;
-use wasm_logger::Config;
-use yew_agent::Threaded;
-fn main() {
-    wasm_logger::init(Config::default());
-    Worker::register();
-}
--- src/state/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod config;
-pub mod state;
-pub mod update;
