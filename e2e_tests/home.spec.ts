719dee442ec12ac0d36c2889b655930d5692a67e
SpanishPear <shrey.somaiya@gmail.com>
2023-09-10 17:24:56 +1000

    update tests for basic output

--- e2e_tests/home.spec.ts
+++ e2e_tests/home.spec.ts
@@ -1,14 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/', { waitUntil: 'load' });
-});
-
 test('has title', async ({ page }) => {
+  await page.goto('/');
+
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/mipsy/);
 });
 
-test('ui meets snapshot', async ({ page }) => {
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 200 });
+test('get started link', async ({ page }) => {
+  await page.goto('/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:8080/',
+    baseURL: 'http://127.0.0.1:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -64,9 +64,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-   webServer: {
+  webServer: {
      command: 'trunk serve',
-     url: 'http://127.0.0.1:8080/',
+     url: 'http://127.0.0.1:8080',
      reuseExistingServer: !process.env.CI,
-   }
+   },
 });
